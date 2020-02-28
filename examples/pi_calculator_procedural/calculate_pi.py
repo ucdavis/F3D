596de807b8cbac7ed07664a7aa31ae5fb9b49cda
Azalee Bostroem <abostroem@gmail.com>
2020-02-28 15:58:43 -0800

    breaking up some lines and adding more comments

--- examples/pi_calculator_procedural/calculate_pi.py
+++ examples/pi_calculator_procedural/calculate_pi.py
@@ -1,10 +1,15 @@
+'''
+This script uses the idea that given a unit circle inside a unit square, the ratio of
+the area of the circle to the area of the square is pi/4
+'''
+
 import numpy as np
 import sys
-
 
 ### Parse Arguments ###
 N_SIG_DIGS = sys.argv[1]
 N_DARTS_PER_THROW = sys.argv[2]
+#Raise exception if too many arguments are passed in
 if len(sys.argv) > 2:
     raise ValueError("Too many values passed!")
 
@@ -12,7 +17,8 @@ if len(sys.argv) > 2:
 ### Setup ###
 threshold = 1 / (10 ** N_SIG_DIGS + 1)
 
-history = list()
+#initialize variables used in while loop
+history = []
 n_darts_thrown = 0
 n_darts_in_circle = 0
 
@@ -22,13 +28,19 @@ converged = False
 while not converged:
     # Throw some darts and add them to the count
     n_darts_thrown += N_DARTS_PER_THROW
+    # Create x,y coordinate pairs for new darts
     coords = np.random(2 * N_DARTS_PER_THROW).reshape(N_DARTS_PER_THROW, 2)
-    n_darts_in_circle += sum(np.linalg.norm(coords, axis=1) < 1)
+    # Find the distance of each dart from the center of the circle
+    distance = np.sqrt(coords[:,0]**2 + coords[:,1]**2)
+    # Add new darts that are inside unit circle to thrown to total
+    n_darts_in_circle += np.size(distance[distance<1])
+    # Recalculate guess based on N_DARTS_PER_THROW more darts
+    pi_guess = 4 * n_darts_in_circle / n_darts_thrown
     # Include the resulting value of `pi` in the history
-    history.append(4 * n_darts_in_circle / n_darts_thrown)
+    history.append(pi_guess)
     # let's say we're converged if the last 100 trials differ
     # by less than threshold.
-    if len(history) < 100:
+    if len(history) < 100: # Don't check for convergence until there are 100 entries in history
         converged = False
     else:
         converged = np.all(abs(np.diff(history[-100:])) < threshold)
