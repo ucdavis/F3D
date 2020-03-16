688399be95bc641eb64da12d3dde754113596f1c
leknox <lknox@ucdavis.edu>
2020-03-16 15:17:08 -0700

    Added comments about how the program works

--- examples/pi_calculator_procedural/calculate_pi.py
+++ examples/pi_calculator_procedural/calculate_pi.py
@@ -8,6 +8,22 @@ N_DARTS_PER_THROW = sys.argv[2]
 if len(sys.argv) > 2:
     raise ValueError("Too many values passed!")
 
+#    Given as input number of significant digits desired and number of darts per "throw", this program calculates pi.
+#
+#    It does so by randomly sampling from the unit square ("throwing darts"), and determining how many of those points
+#    are in the unit circle that has the same center.
+#
+#    At least 100 throws are taken, and the pi estimate is updated each time. The most recent estimate uses the result of all
+#    previous throws. More throws are taken for as long as needed to achieve convergence.
+#
+#    Convergence is achieved when fluctuations in the past 100 calculations of pi are sufficiently small, given the number of
+#    desired significant figures. 
+# 
+#    ----------
+#    N_SIG_DIGS: int
+#        The number of significant digits to calculate pi out to.
+#    N_DARTS_PER_THROW: int
+#        The number of darts thrown at one time, for which an updated calculation is made. 
 
 ### Setup ###
 threshold = 1 / (10 ** N_SIG_DIGS + 1)
